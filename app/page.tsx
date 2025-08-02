7059ab435a71a739b0a95a4f42e1b0382d208f93
rohitkumar91131 <rk34190100@gmail.com>
2025-08-03 02:13:27 +0530

    j

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,45 @@
 "use client";
-import UserList from "./post/AllUser";
-import Button from "./src/Button"
-import Button2 from "./src/Button2";
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 
+const socket = io(process.env.DATABASE_URL);
 
+export default function Page() {
+  const [messages, setMessages] = useState<string[]>([]);
 
-export default function HomePage() {
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("🔗 Connected:", socket.id);
+    });
+
+    socket.on("message", (msg: string) => {
+      setMessages((prev) => [...prev, msg]);
+    });
+
+    return () => {
+      socket.off("message");
+    };
+  }, []);
+
+  const sendMessage = () => {
+    console.log("Send")
+    socket.emit("message", "Hello from Next.js!");
+  };
+
   return (
-    <main className="flex h-screen items-center justify-center">
-      <Button label="Click Me" onClick={() => alert("Hello Next.js + TS")} />
-        <Button2/>
+    <main className="p-6">
+      <h1 className="text-xl font-bold mb-4">⚡ Next.js + Socket.IO</h1>
+      <button
+        onClick={sendMessage}
+        className="px-4 py-2 bg-blue-600 text-white rounded-lg"
+      >
+        Send
+      </button>
+      <ul className="mt-4 space-y-2">
+        {messages.map((msg, i) => (
+          <li key={i} className="p-2 bg-gray-100 rounded text-black">{msg}</li>
+        ))}
+      </ul>
     </main>
   );
 }
